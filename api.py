343a07eaec0a297fdddca26c98d039c28b29c7d0
ronier83 <ronxp@msn.com>
2024-06-13 14:42:46 +0300

    added curl

--- api.py
+++ api.py
@@ -29,3 +29,37 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+
+'''
+curl --location 'https://192.168.27.202/admin/v2/api/metadata/list' \
+--header 'Cookie: JSESSIONID=018DFB1756A1D8BE5FDB0F37C80CB6C7' \
+--header 'Content-Type: application/json' \
+--data '{
+
+    "max_results":2000,
+    "folder_ids" : [40],
+    "cursor":""
+}'
+'''
+
+'''
+curl --location 'https://192.168.27.202/admin/v2/api/metadata/ancestors' \
+--header 'Cookie: JSESSIONID=C3B48E16EC14EBB1FDAFA0D669A50A86' \
+--header 'Content-Type: application/json' \
+--data '{
+    "folder_id": 16,
+    "guid":"4fbc5aca-71d7-4bbf-9928-695d3b2dc711:562"
+}'
+'''
+
+'''
+curl --location 'https://192.168.27.202/admin/v2/api/metadata/longpoll' \
+--header 'Cookie: JSESSIONID=1EB69BACD3A3C56700063DAB7E039E95' \
+--header 'Content-Type: application/json' \
+--data '{
+    "folder_ids": [15, 20],
+  "cursor": "eyJjdXJzb3IiOiJ7XCJjbG91ZEZvbGRlcnNDdXJzb3JcIjo0LFwiZmlsZXNDdXJzb3JcIjpcIk1URTFQVEk2TVRNMk5USTdNakE5TWpvd093PT1cIn0iLCJmb2xkZXJJZHMiOlsyMCwxNV0sInJlc2V0Rm9sZGVySWRzIjpbXX0=",
+  "timeout": "2000"
+}'
+'''
